3a82c80f497b072184c8d90c26dc3fbabbe8a746
codeHusky <lokio27productions@gmail.com>
2018-10-07 01:55:15 -0500

    Clarified the code a bit

--- src/main/java/com/codehusky/huskycrates/injectiontester/Main.java
+++ src/main/java/com/codehusky/huskycrates/injectiontester/Main.java
@@ -9,11 +9,33 @@ import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.plugin.Dependency;
 import org.spongepowered.api.plugin.Plugin;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
 @Plugin(id="hcinjectiontest", name="HuskyCratesInjectionTester", version = "1.0.0", dependencies = {@Dependency(id="huskycrates", version = "2.0.0")})
 public class Main {
     @Listener
     public void inject(CrateInjectionEvent event){
-        Slot testSlot = new Slot(new Item("&7Test", ItemTypes.GOLDEN_SWORD,null,1,null,null,null,null),null,null,1,null,null,null);
+        //TODO: replace this with builder pattern once implemented
+        Slot testSlot = new Slot(
+                new Item( //--// Display Item //--//
+                        "&7Test", // Name
+                        ItemTypes.GOLDEN_SWORD, // Item Type
+                        new ArrayList<>(), // Lore
+                        1, // Count
+                        0, // Metadata/Damage value
+                        0, // Durability value
+                        new ArrayList<>(), // Enchantment data
+                        new LinkedHashMap() // NBT data
+                ), //--// Display Item //--//
+
+                new ArrayList<>(), // Rewards
+                new ArrayList<>(), // Reward Groups
+                1, // Chance integer
+                false, // pickRandom (boolean)
+                1, // pickSize
+                false // pickUnique
+        );
         HuskyCrates.registry.getCrate("injectCrate").injectSlot(testSlot);
     }
 }
